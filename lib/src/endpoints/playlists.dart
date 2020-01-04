92b212afbe93b1b89669c08b48c88e748595829a
Alexandre Cisneiros <alexandre@cisneiros.com>
2020-01-04 14:11:42 -0300

    Add get playlist from ID

--- lib/src/endpoints/playlists.dart
+++ lib/src/endpoints/playlists.dart
@@ -17,6 +17,13 @@ class Playlists extends EndpointPaging {
         (json) => PlaylistsFeatured.fromJson(json));
   }
 
+  Future<Playlist> get(String playlistId) async {
+    var jsonString = await _api._get('v1/playlists/$playlistId');
+    var map = json.decode(jsonString);
+
+    return Playlist.fromJson(map);
+  }
+
   Pages<PlaylistSimple> get me {
     return _getPages('v1/me/playlists', (json) => Playlist.fromJson(json));
   }
